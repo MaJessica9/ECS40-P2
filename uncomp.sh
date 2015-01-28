1f1abf2b35e53173d79db444387f6efa780b7faf
MaJessica9 <jamma@ucdavis.edu>
2015-01-27 22:04:08 -0800

    Update uncomp.sh

--- uncomp.sh
+++ uncomp.sh
@@ -5,12 +5,13 @@ then
   echo usage: uncomp {filelist}+
 else
   while [ $# -gt 0 ] ; do
-  	case $1 in
-  	  *.tar) tar -xf "$1" ;;
-  	  *.tar.gz | *.gz | *.tgz) tar -xzf "$1" ;;
-  	  *.tar.Z | *.Z) tar -xf "$1" ;;
-  	  *.zip) unzip "$1" ;;
-  	  *) echo uncomp: "$1" has no compression extension. ;;
+    case $1 in
+      *.tar) tar -xf "$1" ;;
+      *.tar.gz | *.gz | *.tgz) tar -xzf "$1" ;;
+      *.tar.Z) ;;
+      *.Z) uncompress -cf "$1" ;;
+      *.zip) unzip "$1" >&/dev/null ;; 
+      *) echo uncomp: "$1" has no compression extension. ;;
     esac
     shift
   done # while 
