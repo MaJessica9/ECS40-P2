f6254d32bbabdd73bd0257d3a6bdfd5e7cf4edb7
MaJessica9 <jamma@ucdavis.edu>
2015-01-27 16:14:59 -0800

    Update uncomp.sh

--- uncomp.sh
+++ uncomp.sh
@@ -2,16 +2,15 @@
 
 if [[ $# -eq 0 ]]
 then
-  echo usage: uncomp.sh {filelist}+
+  echo usage: uncomp {filelist}+
 else
-  echo "$1" YEAH
   while [ $# -gt 0 ] ; do
   	case $1 in
   	  *.tar) tar -xf "$1" ;;
   	  *.tar.gz | *.gz | *.tgz) tar -xzf "$1" ;;
   	  *.tar.Z | *.Z) tar -xf "$1" ;;
   	  *.zip) unzip "$1" ;;
-  	  *) echo uncomp.sh: "$1" has no compression extension. ;;
+  	  *) echo uncomp: "$1" has no compression extension. ;;
     esac
     shift
   done # while 
