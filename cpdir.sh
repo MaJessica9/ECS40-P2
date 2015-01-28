93625b610f019f8422c5fb6e276a0670fc8a6428
MaJessica9 <jamma@ucdavis.edu>
2015-01-27 21:46:14 -0800

    Update cpdir.sh

--- cpdir.sh
+++ cpdir.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-if [[ ! -d "$1" || ! -d "$2" || "$#" -ne 3 ]]
+if [[ ! -d "$1" || ! -d "$2" || "$#" -ne 3 || -f "$3" ]]
 then
   echo usage: cpdirs.sh source_directory1 source_directory2 dest_directory
 else
-  mkdir "$3"
-  cp "$1"/* "$3" 2>/dev/null
-  cp -u "$2"/* "$3" 2>/dev/null
+  if [[ ! -d "$3" ]]
+  then
+    mkdir "$3"
+  fi
+  cp --preserve "$1"/* "$3" 2>/dev/null
+  cp -u --preserve "$2"/* "$3" 2>/dev/null
 fi 
