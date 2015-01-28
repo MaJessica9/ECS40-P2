f3df258b3feda75f56dc54ac656f1a302ee6ea10
MaJessica9 <jamma@ucdavis.edu>
2015-01-28 15:34:55 -0800

    Create makemake.sh

---
+++ makemake.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [[ "$#" -lt 1 ]]
+then
+  echo 'Executable name required.'
+  echo 'usage: makemake.sh executable_name'
+else 
+  
+fi
+
+# Helpful Hints
+# Echo Tabs: echo -e ' \t' 
+#   -e: enable interpret of backslash
+#   ' \t': tab 
