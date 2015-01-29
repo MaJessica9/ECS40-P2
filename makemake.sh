68c6f931641f1ee4e5760b2e9e2b3f2f498a4873
MaJessica9 <jamma@ucdavis.edu>
2015-01-28 19:05:18 -0800

    Update makemake.sh

--- makemake.sh
+++ makemake.sh
@@ -4,15 +4,42 @@ if [[ "$#" -eq 0 ]]
 then
   echo Executable name required.
   echo usage: makemake.sh executable_name
-else 
+else 									          	    # initialize Makefile
+  # Line 1
   echo -n "$1 :">Makefile
+  for f in *.cpp ; do                 # finds all .cpp files
+    f2=${f%.*}.o                      # changes them to .o strings
+    echo -n " $f2">>Makefile          # appends the new string to the Makefile
+  done # for
+  echo>>Makefile                      # adds a new line
+
+  # Line 2
+  echo -e -n '\tg++ -ansi -Wall -g -o' "$1">>Makefile
+  for f in *.cpp ; do                 # for every .cpp file
+    f2=${f%.*}.o 
+    echo -n " $f2">>Makefile          # appends the new string to the Makefile
+  done # for
+  echo>>Makefile 
+  echo>>Makefile
+
+  # Line 3+
+  for f in *.cpp ; do                 # for every .cpp file
+    f2=${f%.*}.o
+    echo -n "$f2 :">>Makefile 
+    echo -n " $f">>Makefile
+    awk '/"*.h"/ {print $2}' $f > out.txt # search for .h
+    g=$(sed 's/"//g' out.txt)              # remove the "" in the line
+    echo -n " $g">>Makefile
+    echo>>Makefile  
+  
+    # Line 4+
+    echo -e -n '\tg++ -ansi -Wall -g -c' "$f">>Makefile
+    echo>>Makefile 
+    echo>>Makefile
+    done
+  rm out.txt
 fi
 
-for f in *.sh ; do
-  f2=${f%.*}.o
-  echo -n " $f2">>Makefile
-done # for
-echo>>makefile
 
 
 # Helpful Hints
